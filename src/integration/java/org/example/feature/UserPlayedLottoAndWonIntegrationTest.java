090e186338c0f5886700abfab06b9967c2716ba3
Lucas <lupelikan@int.pl>
2025-03-01 14:54:09 +0200

    added dependencies related to integration tests (TestContainers, MongoDBContainer, WireMock) and created configuration for writing integration tests

---
+++ src/integration/java/org/example/feature/UserPlayedLottoAndWonIntegrationTest.java
@@ -0,0 +1,11 @@
+package org.example.feature;
+
+import org.example.BaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+public class UserPlayedLottoAndWonIntegrationTest extends BaseIntegrationTest {
+    @Test
+    public void shouldUserWinAndSystemShouldGenerateWinners() {
+
+    }
+}
--- src/main/java/org/example/LottoSpringBootApplication.java
+++ src/main/java/org/example/LottoSpringBootApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LottoApp {
+public class LottoSpringBootApplication {
     public static void main(String[] args) {
-        SpringApplication.run(LottoApp.class, args);
+        SpringApplication.run(LottoSpringBootApplication.class, args);
     }
 }
