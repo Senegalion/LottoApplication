cff38b9ba22fb37dcfb8dc80ba857f46c393e031
Lucas <lupelikan@int.pl>
2025-03-04 20:15:45 +0200

    written next step of the integration test, trying to check usage of scheduler using awaitility (but not using database, but just only overriding methods that the test passes)

--- src/integration/java/org/example/feature/UserPlayedLottoAndWonIntegrationTest.java
+++ src/integration/java/org/example/feature/UserPlayedLottoAndWonIntegrationTest.java
@@ -3,10 +3,16 @@ package org.example.feature;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import org.example.BaseIntegrationTest;
 import org.example.domain.winningnumbersgenerator.WinningNumbersGeneratorFacade;
-import org.example.domain.winningnumbersgenerator.dto.WinningNumbersDto;
+import org.example.domain.winningnumbersgenerator.WinningNumbersNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
 
 public class UserPlayedLottoAndWonIntegrationTest extends BaseIntegrationTest {
 
@@ -15,6 +21,7 @@ public class UserPlayedLottoAndWonIntegrationTest extends BaseIntegrationTest {
 
     @Test
     public void shouldUserWinAndSystemShouldGenerateWinners() {
+        // step 1: external service returns 6 random numbers (1,2,3,4,5,6)
         wireMockServer.stubFor(WireMock.get("/api/v1.0/random?min=1&max=99&count=25")
                 .willReturn(WireMock.aResponse()
                         .withStatus(HttpStatus.OK.value())
@@ -23,7 +30,27 @@ public class UserPlayedLottoAndWonIntegrationTest extends BaseIntegrationTest {
                                 [1, 2, 3, 4, 5, 6, 82, 82, 83, 83, 86, 57, 10, 81, 53, 93, 50, 54, 31, 88, 15, 43, 79, 32, 43]
                                           """.trim()
                         )));
-        WinningNumbersDto winningNumbersDto = winningNumbersGeneratorFacade.generateWinningNumbers();
-        System.out.println(winningNumbersDto);
+        // step 2: system fetched winning numbers for draw date: 8.03.2025 12:00
+        LocalDateTime drawDate = LocalDateTime.of(2025, 3, 8, 12, 0, 0);
+        await()
+                .atMost(20, TimeUnit.SECONDS)
+                .pollInterval(Duration.ofSeconds(1))
+                .until(() -> {
+                    try {
+                        return !winningNumbersGeneratorFacade
+                                .retrieveWinningNumberByDate(drawDate)
+                                .winningNumbers()
+                                .isEmpty();
+                    } catch (WinningNumbersNotFoundException exception) {
+                        return false;
+                    }
+                });
+
+        // step 3: user made POST /inputNumbers with 6 numbers (1, 2, 3, 4, 5, 6) at 4-03-2025 10:00 and system returned OK(200) with message: “success” and Ticket (DrawDate:8.03.2025 12:00 (Saturday), TicketId: sampleTicketId)
+        // step 4: 3 days and 1 minute passed, and it is 1 minute after the draw date (8.03.2025 12:01)
+        // step 5: system generated result for TicketId: sampleTicketId with draw date 8.03.2025 12:00, and saved it with 6 hits
+        // step 6: 3 hours passed, and it is 1 minute after announcement time (8.03.2025 15:01)
+        // step 7: user made GET /results/sampleTicketId and system returned 200 (OK)
+
     }
 }
